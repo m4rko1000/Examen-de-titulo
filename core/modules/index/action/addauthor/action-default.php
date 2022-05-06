fa304bdcf5ce13a7835ddb240a9d805f56c29a4b
m4rko1000 <marko_antonio11@hotmail.com>
2022-05-05 23:52:20 -0400

    v2

--- core/modules/index/action/addauthor/action-default.php
+++ core/modules/index/action/addauthor/action-default.php
@@ -1,6 +1,6 @@
 <?php
 
-if(count($_POST)>0){
+if (count($_POST) > 0) {
 	$user = new AuthorData();
 	$user->name = $_POST["name"];
 	$user->lastname = $_POST["lastname"];
@@ -9,10 +9,5 @@ if(count($_POST)>0){
 	$user->precio = $_POST["precio"];
 	$user->add();
 
-print "<script>window.location='index.php?view=authors';</script>";
-
-
+	print "<script>window.location='index.php?view=authors';</script>";
 }
-
-
-?>
